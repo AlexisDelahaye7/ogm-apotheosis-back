4bc210a8a6282178197fbe846454ca0c5018949f
KevinBournique <kevin.bournique@oclock.io>
2023-08-23 11:41:55 +0200

    build: New road for login

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -1,10 +1,33 @@
 import express from 'express';
 // import scenarioRouter from './scenario.router.js';
+import jwt from 'jsonwebtoken';
 import { errorHandler } from '../middlewares/error.middleware.js';
 
 const router = express.Router();
 
 router.use('/', (req, res) => { res.send('coucou'); });
+
+const users = [
+  {
+    username: 'admin',
+    password: 'admin',
+  },
+];
+
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const valid = users.some((user) => user.username === username && user.password === password);
+
+  const token = jwt.sign({ username }, process.env.PRIVATE_KEY, { algorithm: 'RS256' });
+
+  if (valid) {
+    res.send(token);
+  } else {
+    res.status(404).send('Not ok');
+  }
+});
 // router.use('/scenario', scenarioRouter);
 
 router.use(errorHandler);
