9332c3abbeb2b6ccd4d08833afd9e415e8533396
KevinBournique <kevin.bournique@oclock.io>
2023-08-23 14:14:23 +0200

    feat: update file index.router.js

--- app/routers/index.router.js
+++ app/routers/index.router.js
@@ -6,10 +6,9 @@ import { errorHandler } from '../middlewares/error.middleware.js';
 
 const router = express.Router();
 
+router.use('/auth', login);
+
 router.use('/', (req, res) => { res.send('coucou'); });
-
-router.use('/login', login);
-
 // router.use('/scenario', scenarioRouter);
 
 router.use(errorHandler);
