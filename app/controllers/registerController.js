e0ec0ef9f5b3fd3ee35dc57aa4835f27611d6106
KevinBournique <kevin.bournique@oclock.io>
2023-08-25 13:30:43 +0200

    delete fisl register

--- app/controllers/registerController.js
+++
@@ -1,2 +0,0 @@
-import { ApiError } from '../middlewares/error.middleware.js';
-import registerDatamapper from '../models/register.datamapper.js';
--- app/routers/auth.router.js
+++ app/routers/auth.router.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import registerController from '../controllers/registerController.js';
 import loginController from '../controllers/loginController.js';
 
 const router = express.Router();
 
 // route "/auth"
-
-router
-  .route('/register')
-  .post(registerController.createUser);
 
 router.post('/', loginController.login);
 
