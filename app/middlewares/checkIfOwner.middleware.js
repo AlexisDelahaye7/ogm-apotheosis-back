e21fa9441c0a1451f710a764d2acccb833b39e1d
Alexis Delahaye <alexis.delahaye7@gmail.com>
2023-08-28 14:44:59 +0200

    hotfix: req.params.id type fix

--- app/middlewares/checkIfOwner.middleware.js
+++ app/middlewares/checkIfOwner.middleware.js
@@ -2,7 +2,7 @@ import logger from '../helpers/logger.js';
 import findRoleByUserPk from '../models/role.datamapper.js';
 
 export default async function checkIfOwner(req, res, next) {
-  if (req.user.id !== req.params.id) return res.status(401).json({ message: 'Unauthorized' });
+  if (req.user.id !== Number(req.params.id)) return res.status(401).json({ message: 'Unauthorized' });
 
   // ! sinon, next(new ApiError('Unauthorized', { statusCode: 401 }));
 
