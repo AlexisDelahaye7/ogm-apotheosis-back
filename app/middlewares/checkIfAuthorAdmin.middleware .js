b8db3034d2ef55fbbadccc84e7fc684af9493fc2
KevinBournique <kevin.bournique@oclock.io>
2023-08-31 10:08:15 +0200

    fix: findById => findByPk

--- app/middlewares/checkIfAuthorAdmin.middleware .js
+++ app/middlewares/checkIfAuthorAdmin.middleware .js
@@ -5,7 +5,7 @@ import scenarioDatamapper from '../models/scenario.datamapper.js';
 
 export default async function checkIfAuthor(req, res, next) {
   const userRole = await roleDatamapper.findRoleByUserPk(req.user.id);
-  const result = await scenarioDatamapper.findById(req.params.id);
+  const result = await scenarioDatamapper.findByPk(req.params.id);
   req.user.auth_level = userRole.auth_level;
 
   if (req.user.auth_level < 3 && req.user.id !== result.author_id) return res.status(401).json({ message: 'Unauthorized' });
