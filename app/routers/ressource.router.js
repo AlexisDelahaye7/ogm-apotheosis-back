ac9f4cda23584e438ee5f8f8a10f84629f43de1e
Alexis Delahaye <alexis.delahaye7@gmail.com>
2023-09-01 13:44:15 +0200

    done schema validation

--- app/routers/ressource.router.js
+++ app/routers/ressource.router.js
@@ -2,6 +2,8 @@ import express from 'express';
 import ressourceController from '../controllers/ressource.controller.js';
 import getUserToken from '../middlewares/jwt.middleware.js';
 import controllerHandler from '../middlewares/controller.middleware.js';
+import createSchema from '../validation/schemas/ressource.create.schema.js';
+import updateSchema from '../validation/schemas/ressource.update.schema.js';
 
 import validate from '../middlewares/validator.middleware.js';
 
